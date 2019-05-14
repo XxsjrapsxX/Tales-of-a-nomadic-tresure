b4190219e1f55280ce284aa4ebe1bcfd893ae218
XxsjrapsxX <44506272+XxsjrapsxX@users.noreply.github.com>
2019-05-14 12:49:21 -0400

    Move mad for slime

--- src/com/company/enemies/Bosses/theGreatSlimeKing.java
+++ src/com/company/enemies/Bosses/theGreatSlimeKing.java
@@ -5,42 +5,42 @@ import com.company.character;
 
 import gameutil.text.Console;
 
-public class theGreatSlimeKing extends Enemy{
+public class theGreatSlimeKing extends Enemy {
 
     public theGreatSlimeKing(int x, int y) {
         super(x, y);
-        name="The theGreatSlimeKing";
-        race="Half-elf";
-        maxHealth=125;
-        health=125;
-        dex=9;
-        atk=17;
-        charisma=0;
-        magAtk=16;
-        karma=10;
-        focus=8;
-        accuracy=10;
-        def=8;
-        magDef=0;
+        name = "The theGreatSlimeKing";
+        race = "Half-elf";
+        maxHealth = 125;
+        health = 125;
+        dex = 9;
+        atk = 17;
+        charisma = 0;
+        magAtk = 16;
+        karma = 10;
+        focus = 8;
+        accuracy = 10;
+        def = 20;
+        magDef = 20;
     }
 
-    public void doubleSlash(){
-        Console.s.println("The rogue raised the first dagger and cut vertically");
-        Main.player.health-=atk *1.5;
-        Console.s.println("The rogue raised the second dagger and cut horizontally");
-        Main.player.health-=atk *1.5;
+    public void slimeSpikeVolley() {
+        Console.s.println("The great slime king sent slime flying");
+        Main.player.health -= atk * 1;
+        Main.player.health -= atk * 1;
+        Main.player.health -= atk * 1;
     }
 
     public void decideAttack(character guy) {
-        int atkType=Main.rand.nextInt(3);
-        if (atkType==0) {
+        int atkType = Main.rand.nextInt(3);
+        if (atkType == 0) {
             attack(guy);
-        } else if(atkType == 1) {
-            doubleSlash();
+        } else if (atkType == 1) {
+            slimeSpikeVolley();
         }
     }
 
     public void act(){
-        moveToward(Main.player,2);
+        moveToward(Main.player, 0);
     }
 }
